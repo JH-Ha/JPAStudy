a9bda9e98730f78a768b96183555e45bc898cb7e
JH-Ha <cubeprince@gmail.com>
2022-07-31 14:41:39 +0900

    Add gradle setting of Kotlin querydsl

--- QueryDslStudy/build.gradle.kts
+++ QueryDslStudy/build.gradle.kts
@@ -1,34 +1,56 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.7.2"
-	id("io.spring.dependency-management") version "1.0.12.RELEASE"
-	kotlin("jvm") version "1.6.21"
-	kotlin("plugin.spring") version "1.6.21"
+    id("org.springframework.boot") version "2.7.0"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    kotlin("jvm") version "1.6.21"
+    kotlin("plugin.spring") version "1.6.21"
+    kotlin("plugin.jpa") version "1.6.21"
+    kotlin("kapt") version "1.6.21"
 }
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
+java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    apply(plugin = "kotlin-kapt")
+
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlin:kotlin-noarg:1.6.21")
+    implementation("com.h2database:h2")
+
+    implementation("com.querydsl:querydsl-jpa:5.0.0")
+    implementation(group = "com.querydsl", name = "querydsl-apt", version = "5.0.0")
+
+    api("com.querydsl:querydsl-jpa")
+    kapt(group = "com.querydsl", name = "querydsl-apt", classifier = "jpa")
+
+    runtimeOnly("mysql:mysql-connector-java")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+sourceSets["main"].withConvention(org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet::class) {
+    kotlin.srcDir("$buildDir/generated/source/kapt/main")
+
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "17"
-	}
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
--- QueryDslStudy/src/test/kotlin/com/example/demo/DemoApplicationTests.kt
+++ QueryDslStudy/src/test/kotlin/com/example/demo/DemoApplicationTests.kt
@@ -1,13 +1,35 @@
 package com.example.demo
 
+import com.example.demo.entity.Hello
+import com.example.demo.entity.QHello
+import com.querydsl.jpa.impl.JPAQueryFactory
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.transaction.annotation.Transactional
+import javax.persistence.EntityManager
 
 @SpringBootTest
+@Transactional
 class DemoApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Autowired
+    lateinit var em: EntityManager
+
+    @Test
+    fun contextLoads() {
+        var hello = Hello()
+        em.persist(hello)
+
+        var query = JPAQueryFactory(em)
+        var qHello = QHello("h")
+
+        var result: Hello? = query.selectFrom(qHello)
+            .fetchOne()
+
+        Assertions.assertThat(result).isEqualTo(hello)
+        Assertions.assertThat(result?.id).isEqualTo(hello.id)
+    }
 
 }
